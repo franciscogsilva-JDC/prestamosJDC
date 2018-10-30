884ffa057352f2983e5e096ca7cf6bcfdcb63e4b
Francisco GSilva <javiergonzalezsilva@gmail.com>
2018-10-30 10:56:10 -0500

    WEB - Index updated

--- backend_web/prestamosJDC/resources/views/front/index.blade.php
+++ backend_web/prestamosJDC/resources/views/front/index.blade.php
@@ -2,22 +2,50 @@
 
 @section('content')
 	@include('admin.layouts.partials._messages')
-	<div class="parallax-container center valign-wrapper">
-		<div class="parallax">
-			<img src="https://images.pexels.com/photos/356065/pexels-photo-356065.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260">
+	<div class="section no-pad-bot" id="index-banner">
+		<div class="container">
+			<br><br>
+			<h1 class="header center color-index">{{ env('APP_NAME') }}</h1>
+			<div class="row center">
+				<h5 class="header col s12 light">Sistema de la Fundación Universitaria Juan de Castellanos para la solicitud de espacios y recursos</h5>
+			</div>
+			<div class="row center">
+				<a href="{{ route('requests-front.create') }}" id="download-button" class="btn-large waves-effect waves-light btn-fgs-show">Nueva Solicitud</a>
+			</div>
+			<br><br>
+
 		</div>
 	</div>
-	<div class="section white">
-		<div class="center-align">
-			<a href="{{ route('requests-front.create') }}" class="btn waves-effect btn-bts-edit btn-login btn-fgs-show">Nueva Solicitud</a>			
+	<div class="container">
+		<div class="section">
+			<!--   Icon Section   -->
+			<div class="row">
+				<div class="col s12 m4">
+					<div class="icon-block">
+						<h2 class="center color-index"><i class="material-icons">flash_on</i></h2>
+						<h5 class="center">Solicitudes en linea</h5>
+
+						<p class="light">Agilizamos el proceso de solicitud de espacio físico y/o recurso, ya sea deporte, audiovisual y otros.</p>
+					</div>
+				</div>
+				<div class="col s12 m4">
+					<div class="icon-block">
+						<h2 class="center color-index"><i class="material-icons">group</i></h2>
+						<h5 class="center">User Experience Focused</h5>
+
+						<p class="light">By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
+					</div>
+				</div>
+				<div class="col s12 m4">
+					<div class="icon-block">
+						<h2 class="center color-index"><i class="material-icons">settings</i></h2>
+						<h5 class="center">Administra</h5>
+
+						<p class="light">Administra las solicitudes, así como revisa el historial de solicitudes de tu cuenta.</p>
+					</div>
+				</div>
+			</div>
 		</div>
+		<br><br>
 	</div>
 @endsection()
-
-@section('js')
-	<script type="text/javascript">
-		$(document).ready(function(){
-	      	$('.slider').slider();
-	    });
-	</script>
-@endsection
