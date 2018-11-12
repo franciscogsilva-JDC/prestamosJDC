f498a6b360b503bc6bc821dffa6694e93736e6b3
franciscogsilva <javiergonzalezsilva@gmail.com>
2018-11-12 14:42:56 -0500

    Unit tests

---
+++ backend_web/prestamosJDC/app/Mail/TestEmail.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Mail;
+
+use App\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class TestEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $message;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('emails.testEmail')
+            ->subject('Correo de prueba - '.env('APP_NAME'));
+    }
+}
